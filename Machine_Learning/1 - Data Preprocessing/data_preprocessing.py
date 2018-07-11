04b9ecb636df94634ba6441d1c235c56c81e78b6
Patrik Zelena <zpatrik94@gmail.com>
2018-07-11 14:48:02 +0200

    Comment added to One Hot encode

--- Machine_Learning/1 - Data Preprocessing/data_preprocessing.py
+++ Machine_Learning/1 - Data Preprocessing/data_preprocessing.py
@@ -1,7 +1,6 @@
 # Importing the libraries
 import pandas as pd
 from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -16,12 +15,16 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Encoding Independant Variable
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 
+# Encoding Dependant Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
